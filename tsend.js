f8a23dc1942148bd8ffa7a3a52a6a5e341aa3f02
Lee Treveil <leetreveil@gmail.com>
2013-02-23 21:45:58 +0000

    add jasmine test reporter

--- tsend.js
+++ tsend.js
@@ -143,6 +143,73 @@ var testFrameworks = {
                 submit(results);
             };
         }
+    },
+
+    'jasmine': {
+        detect: function () {
+            return typeof jasmine !== 'undefined';
+        },
+        install: function () {
+            var results = {
+                failed: 0,
+                passed: 0,
+                total: 0,
+                tests: []
+            };
+
+            function JasmineAdapterReporter () {};
+
+            JasmineAdapterReporter.prototype.reportSpecResults = function (spec) {
+                if (spec.results().skipped) {
+                    return;
+                }
+
+                var test = {
+                    passed: 0,
+                    failed: 0,
+                    total: 0,
+                    id: spec.id + 1,
+                    name: spec.getFullName(),
+                    items: []
+                };
+                
+                var items = spec.results().getItems();
+                
+                for (var i = 0, len = items.length; i < len; i++) {
+                    var item = items[i];
+                    if (item.type === 'log') { 
+                        continue;
+                    }
+                    var passed = item.passed();
+                    test.total++;
+                    if (passed) {
+                        test.passed++;
+                    } else {
+                        test.failed++;
+                    }
+                    test.items.push({
+                        passed: passed,
+                        message: item.message,
+                        stacktrace: item.trace.stack ? item.trace.stack : undefined
+                    });
+                }
+
+                results.tests.push(test);
+
+                results.total++;
+                if (test.failed > 0) {
+                    results.failed++;
+                } else {
+                    results.passed++;
+                }
+            }
+
+            JasmineAdapterReporter.prototype.reportRunnerResults = function (runner) {
+                submit(results);
+            }
+
+            jasmine.getEnv().addReporter(new JasmineAdapterReporter);
+        }
     }
 }
 detectAndInstall();

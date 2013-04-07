b94e00518d63d629404ad8d01d060290bb2a8b00
Lee Treveil <leetreveil@gmail.com>
2013-04-07 16:00:03 -0500

    cleanup tests

--- test/test-tap.js
+++ test/test-tap.js
@@ -4,13 +4,27 @@ var tapOut = require('../lib/tap');
 
 test('should convert json object to tap output', function (t) {
     t.plan(1);
-    var expected = '\n1..0\nTAP version 13\n# tests 0\n\n# ok\n'
+    var expected =
+'\n\
+1..0\n\
+TAP version 13\n\
+# tests 0\n\
+\n\
+# ok\n'
     t.equal(expected, tapOut({'tests': []}));
 });
 
 test('should output ok row for test when failed is false', function (t) {
     t.plan(1);
-    var expected = 'TAP version 13\nok 1 haggis\n\n1..1\n# tests 1\n# pass  1\n\n# ok\n';
+    var expected =
+'TAP version 13\n\
+ok 1 haggis\n\
+\n\
+1..1\n\
+# tests 1\n\
+# pass  1\n\
+\n\
+# ok\n';
     t.equal(expected, tapOut({'tests': [{'name': 'haggis', 'failed': false}]}));
 });
 

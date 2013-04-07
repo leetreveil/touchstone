6484fe682ad88891749504e53c1860ee2cf1db49
Lee Treveil <leetreveil@gmail.com>
2013-04-07 15:56:06 -0500

    output test failure details in TAP

--- test/test-tap.js
+++ test/test-tap.js
@@ -13,3 +13,20 @@ test('should output ok row for test when failed is false', function (t) {
     var expected = 'TAP version 13\nok 1 haggis\n\n1..1\n# tests 1\n# pass  1\n\n# ok\n';
     t.equal(expected, tapOut({'tests': [{'name': 'haggis', 'failed': false}]}));
 });
+
+test('should output error source when test has failed', function (t) {
+    t.plan(1);
+    var input = {"failed":1,"passed":0,"total":1,"tests":[{"id":1,"name":"abc","items":[{"passed":false,"actual":false,"expected":true,"source":"error_source"}],"failed":1,"passed":0,"total":1}]};
+    var expected =
+'TAP version 13\n\
+not ok 1 abc\n\
+  ---\n\
+    message: ~\n\
+    source:  error_source\n\
+  ...\n\
+\n\
+1..1\n\
+# tests 1\n\
+# fail  1\n'
+    t.equal(expected, tapOut(input));
+});
